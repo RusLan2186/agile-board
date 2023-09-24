9e7b9bb41a97c7beef1a6c70faf2e9855fb7012d
Ruslan <ruslanchikbest@gmail.com>
2023-09-24 13:30:21 +0300

    Create description for Users.js

--- src/store/users.js
+++ src/store/users.js
@@ -1,5 +1,25 @@
-import { types } from 'mobx-state-tree';
+import { flow, types } from 'mobx-state-tree';
 
-const UsersStore = types.model('UsersStore', {});
+const User = types.model('User', {
+  id: types.identifier,
+  createdAt: types.string,
+  name: types.string,
+  avatar: types.string,
+});
+
+const UsersStore = types
+  .model('UsersStore', {
+    users: types.maybe(types.array(User)),
+  })
+  .actions((self) => {
+    return {
+      load: flow(function* () {
+        self.users = yield apiCall.get('users');
+      }),
+      afterCreate() {
+        self.load();
+      },
+    };
+  });
 
 export default UsersStore;
